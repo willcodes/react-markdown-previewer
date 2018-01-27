3f4a84fc76efb3d0bc9cfcdf92851791fb341505
willcodes <difruscio.dev@gmail.com>
2018-01-27 11:53:49 -0500

    added error handling to server

--- server.js
+++ server.js
@@ -1,51 +1,52 @@
-const express = require('express'),
-      path = require('path'),
-      fs = require('fs'),
-      bodyParser = require('body-parser'),
-      redis = require('redis'),
-      client = redis.createClient(),
-      app = express(),
-      cors = require('cors'),
-      port = process.env.PORT || 3003,
-      publicPath = path.resolve(__dirname);
+const express = require("express"),
+  path = require("path"),
+  fs = require("fs"),
+  bodyParser = require("body-parser"),
+  redis = require("redis"),
+  client = redis.createClient(),
+  app = express(),
+  cors = require("cors"),
+  port = process.env.PORT || 3003,
+  publicPath = path.resolve(__dirname);
 
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-
-app.get('/favicon.ico', function (req, res) {
-});
-
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.get("/favicon.ico", function(req, res) {});
 app.use(bodyParser.json());
+//just for dev localhost, must remove for prod
+app.use(cors({ origin: true, credentials: true }));
 
-app.use(cors({origin:true,credentials: true}));
 
-//endpoint to get get key based on query string
-app.get('/:id', (req,res) => {
+app.get("/:id", (req, res) => {
   let documentKey = req.params.id;
   client.get(documentKey, (err, reply) => {
-    if(reply) {
-      res.send(reply);
+    if (err) {
+      res.status(500).send(err);
+      console.log(err);
+    } else {
+      if (reply) {
+        res.send(reply);
+      } else {
+        res.send("");
+      }
     }
-    else {
-      res.send("")
-    }
-  })
+  });
 });
 
-//endpoint to save to redis
-app.post('/save', (req, res) => {
-  client.set(req.body.docName, req.body.docContent,(err, reply) => {
-    if(err) {
-      res.send(err)
-    } 
-    else {
+app.post("/save", (req, res) => {
+  client.set(req.body.docName, req.body.docContent, (err, reply) => {
+    if (err) {
+      res.status(500).send(err);
+      console.log(err);
+    } else {
       res.send(reply);
     }
-  })
+  });
 });
 
-// And run the server
-app.listen(port, function () {
-  console.log('Server running on port ' + port);
+app.listen(port, function() {
+  console.log("Server running on port " + port);
 });
