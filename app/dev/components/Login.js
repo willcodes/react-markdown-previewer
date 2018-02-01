ce454bda3afdad9f51f4724d1870ce068301ab2d
Meagan Pau <meagan.pau@gmail.com>
2018-02-01 16:06:57 -0500

    style login page

--- app/dev/components/Login.js
+++ app/dev/components/Login.js
@@ -1,45 +1,66 @@
 import React from "react";
 import TextField from "material-ui/TextField";
 import RaisedButton from "material-ui/RaisedButton";
-import { orange500, blue500 } from "material-ui/styles/colors";
+import FlatButton from "material-ui/FlatButton";
+import Paper from 'material-ui/Paper';
+import { orange500, blue900 } from "material-ui/styles/colors";
 import { Link } from "react-router-dom";
 
-const styles = {
+const inputStyles = {
   errorStyle: {
     color: "red"
   },
   underlineStyle: {
-    borderColor: blue500
+    borderColor: blue900
   },
   floatingLabelStyle: {
     color: orange500
   },
   floatingLabelFocusStyle: {
-    color: blue500
-  }
+    color: blue900
+  },
 };
 
 export default class Login extends React.Component {
   render() {
     return (
-      <div>
-        <br />
-        <TextField
-          hintText="Username"
-          errorText="This field is required."
-          errorStyle={styles.errorStyle}
-        />
-        <br />
-        <TextField
-          hintText="Password"
-          underlineStyle={styles.underlineStyle}
-          type="password"
-        />
-        <br />
-        <RaisedButton label="Sign In" primary={true} />
-        <Link to="/signup">
-          <p> sign up </p>
-        </Link>
+      <div className="container">
+        <Paper 
+          zDepth={2}
+          className="login-container"
+        >
+          <h2>Markdown Pad Login</h2>
+          <TextField
+            floatingLabelText="Username"
+            errorText="Please enter your username"
+            underlineFocusStyle={{borderColor: "#364459"}}
+            floatingLabelFocusStyle={{color: "#7589a3"}}
+            fullWidth={true}
+            />
+          <br />
+          <TextField
+            floatingLabelText="Password"
+            type="password"
+            underlineFocusStyle={{borderColor: "#364459"}}
+            floatingLabelFocusStyle={{color: "#7589a3"}}
+            fullWidth={true}
+          />
+          <br />
+          <br />
+          <RaisedButton 
+            label="Sign In" 
+            fullWidth={true}
+            backgroundColor="#364459"
+            labelColor="white"
+          />
+          <br />
+          <Link to="/signup">
+            <FlatButton 
+              label="Register" 
+              fullWidth={true}              
+            />
+          </Link>
+          </Paper>
       </div>
     );
   }
--- app/dev/components/Modal.js
+++ app/dev/components/Modal.js
@@ -1,15 +1,41 @@
 import React from "react";
 import CopyToClipboard from "react-copy-to-clipboard";
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import Dialog from 'material-ui/Dialog';
 
-const Modal = ({ modalText, modalClass }) => {
-  return (
-    <div id="popupModal" className={modalClass}>
-      <p>{modalText}</p>
-      <CopyToClipboard text={window.location.href}>
-        <button className="copyButton">Click to copy link</button>
-      </CopyToClipboard>
-    </div>
-  );
-};
+export default class Modal extends React.Component {
 
-export default Modal;
+  render() {
+
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        onClick={this.props.close}
+        primary={true}
+        className="cancel-btn"
+      />
+    ];
+
+    return (
+      <Dialog
+        className="modal-container"
+        title={this.props.title}
+        actions={actions}
+        modal={false}
+        open={this.props.open}
+        onRequestClose={this.props.close}
+      >
+        <pre>{window.location.href}</pre>
+        <CopyToClipboard text={window.location.href}>
+          <RaisedButton 
+            className="copy-link-btn"
+            label="Copy link" 
+            labelColor="#eee"
+            backgroundColor="#364459"
+          />
+        </CopyToClipboard>
+      </Dialog>
+    );
+  }
+}
